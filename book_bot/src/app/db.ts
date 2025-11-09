0e345c9fd9f2609a63db451efcd3098902a28466
ZhekichCherep <belousoff.zhencka@yandex.ru>
2025-11-09 20:05:13 +0300

    Change db connection

--- book_bot/src/app/db.ts
+++ book_bot/src/app/db.ts
@@ -1,9 +1,13 @@
 import { Pool } from 'pg';
 
+const DB_NAME = 'book_room';
+const DB_USERNAME = 'postgres';
+const DB_PASSWORD = '1234';
+
 export const db = new Pool({
-  user: 'postgres',
-  password: '1234',
-  host: 'localhost',
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  host: 'db',
   port: 5432,
-  database: 'book_room',
+  database: DB_NAME,
 });
